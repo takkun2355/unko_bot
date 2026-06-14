d32a0a478f034808acfe3d35d53e85e331c42b93
Akikukeo1 (Keisuke Ishii) <256033008+Akikukeo1@users.noreply.github.com>
2026-06-14 10:54:49 +0900

    不要なファイルを削除

--- .bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -NoExit -Command ^
-"& 'C:/Users/takku/Documents/bots/unko/venv/Scripts/Activate.ps1'; ^
-& 'C:/Users/takku/Documents/bots/unko/venv/Scripts/python.exe' 'C:/Users/takku/Documents/bots/unko/bot_manager.py'"
